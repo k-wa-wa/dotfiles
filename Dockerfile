6d4ff7542f755eb77e1d25d95bb32992def5dbbe
watanabe <sek.ohei.w0822@icloud.com>
2025-01-01 16:28:14 +0900

    update: nvimを追加、権限周りを修正

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,19 @@
 FROM ubuntu:24.04
 
 RUN apt update && apt -y upgrade
-RUN apt install -y git curl
+RUN apt install -y git curl sudo
+
+RUN usermod -aG sudo ubuntu \
+  && echo "ubuntu ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/ubuntu
 
 # 開発用
 RUN apt install -y vim
 
-WORKDIR /root/.dotfiles
+USER ubuntu
+WORKDIR /home/ubuntu/.dotfiles
+
 COPY . .
 RUN /bin/sh scripts/setup.sh
 
-CMD [ "tail", "-f", "/dev/null" ]
+WORKDIR /home/ubuntu
+CMD [ "zsh" ]
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
-apt install -y \
-    curl zsh lsd
+set -e
+
+sudo apt install -y \
+    zsh lsd build-essential \
+    neovim fzf
 
 # zinit
 bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 
 # starship
-curl -sS https://starship.rs/install.sh | sh -s -- --yes
+curl -sS https://starship.rs/install.sh | sudo sh -s -- --yes
 
 # fastfetch TODO
+
+# TODO ちゃんと書く
+USER=$(whoami)
+GROUP=$(id -gn)
+sudo chown -R $USER:$GROUP ~/.dotfiles/.config
 
 # link
 ln -sf ~/.dotfiles/.zshrc ~
