833ca1f3f38613b83d5c59f9b9aa3f0f72f78cf9
watanabe <sek.ohei.w0822@icloud.com>
2025-01-06 19:17:37 +0900

    update: install時、ディレクトリが既にあれば最新化する

--- install.sh
+++ install.sh
@@ -3,8 +3,12 @@
 DOTFILES_DIR=~/.dotfiles
 
 if [ ! -d ${DOTFILES_DIR} ]; then
-    git clone https://github.com/k-wa-wa/dotfiles ${DOTFILES_DIR}
+  git clone https://github.com/k-wa-wa/dotfiles ${DOTFILES_DIR}
+else
+  cd ${DOTFILES_DIR}
+  git fetch origin
+  git checkout origin/master
 fi
 
-cd ~/.dotfiles
+cd ${DOTFILES_DIR}
 /bin/sh scripts/setup.sh
